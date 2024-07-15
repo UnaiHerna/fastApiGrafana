ea26d6fdc5eecb3c4fc898688e7f0912dfaa16bf
UnaiHernandezIzcue <uhernandez@cimico.tech>
2024-07-15 12:25:22 +0200

    Variable por equipo creado

--- routers/sensor.py
+++ routers/sensor.py
@@ -84,6 +84,32 @@ def read_datos_sensor_multiple_by_variable(db, variables):
     return all_data
 
 
+def read_datos_sensor_variable_by_equipo(db, variable, equipo):
+    cache_key = f"datos_sensor_{variable}_{equipo}"
+    cached_data = get_cached_response(cache_key)
+    if cached_data:
+        return cached_data
+
+    query = (
+        select(
+            SensorDatos.timestamp.label('time'),
+            SensorDatos.valor.label('value'),
+            Variable.simbolo.label('variable'),
+            Equipo.descripcion.label('equipo')
+        )
+        .join(Sensor, (SensorDatos.id_equipo == Sensor.id_equipo) & (SensorDatos.id_variable == Sensor.id_variable))
+        .join(Variable, Sensor.id_variable == Variable.id)
+        .join(Equipo, Sensor.id_equipo == Equipo.id)
+        .where((Variable.simbolo == variable) & (Equipo.nombre == equipo))
+        .order_by(SensorDatos.timestamp.asc())
+    )
+    resultados = db.execute(query).fetchall()
+    datos = [{"time": r.time, "value": r.value, "variable": r.variable, "equipo": r.equipo} for r in resultados]
+
+    set_cached_response(cache_key, datos)
+    return datos
+
+
 @router.get("/")
 def datos_condicionales_sensor(
         variable: Optional[str] = None,
@@ -92,16 +118,24 @@ def datos_condicionales_sensor(
         equipos: Optional[str] = None,
         db: Session = Depends(get_db)
 ):
-    if variable and not equipo and not variables and not equipos:
-        # Lógica para manejar la solicitud cuando solo se proporciona el símbolo
-        return read_datos_sensor_by_variable(db, variable)
-    elif equipo and not variable and not variables and not equipos:
-        # Lógica para manejar la solicitud cuando solo se proporciona el nombre
-        return read_datos_sensor_by_equipo(db, equipo)
-    elif variables and not equipo and not variable and not equipos:
-        return read_datos_sensor_multiple_by_variable(db, variables)
-    elif equipos and not equipo and not variable and not variables:
-        return read_datos_sensor_multiple_by_equipos(db, equipos)
-    else:
-        # Lógica para manejar la solicitud cuando no se proporciona ninguno de los parámetros esperados
-        raise HTTPException(status_code=400, detail="Debe proporcionar los datos de forma correcta.")
+    match (variable, equipo, variables, equipos):
+        case (variable, None, None, None) if variable:
+            return read_datos_sensor_by_variable(db, variable)
+
+        case (None, equipo, None, None) if equipo:
+            return read_datos_sensor_by_equipo(db, equipo)
+
+        case (None, None, variables, None) if variables:
+            return read_datos_sensor_multiple_by_variable(db, variables)
+
+        case (None, None, None, equipos) if equipos:
+            return read_datos_sensor_multiple_by_equipos(db, equipos)
+
+        case(variable, equipo, None, None) if variable and equipo:
+            return read_datos_sensor_variable_by_equipo(db, variable, equipo)
+
+        case (None, None, None, None):
+            raise HTTPException(status_code=400, detail="Debe proporcionar al menos un parámetro.")
+
+        case (_, _, _, _):
+            raise HTTPException(status_code=404, detail="No existe esa combinación.")
